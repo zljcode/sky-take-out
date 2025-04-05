11604b50cbb6ad3db8021cdac7a6e48c1149cac9
zljcode <1393300842@qq.com>
2025-04-05 17:02:58 +0800

    清空购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceimpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceimpl.java
@@ -92,4 +92,13 @@ public class ShoppingCartServiceimpl implements ShoppingCartService {
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         return list;
     }
+
+    /**
+     * 清空购物车
+     */
+    public void cleanShoppingCart(){
+        //获取当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        shoppingCartMapper.deleteByUserId(userId);
+    }
 }
